43ebb059bab64e676c033f691f79f0cf2d4e8ce2
YounusMahr <younissmahar@gmail.com>
2026-04-11 20:37:10 +0500

    updated

--- resources/views/livewire/grocery/cash/dashboard.blade.php
+++ resources/views/livewire/grocery/cash/dashboard.blade.php
@@ -10,22 +10,22 @@
          x-transition:enter-end="opacity-100 transform scale-100">
         <!-- row 1 -->
         <div class="flex flex-wrap -mx-3">
-          <!-- Total Cash Added -->
+          <!-- Total Remaining -->
           <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
             <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
               <div class="flex-auto p-4">
                 <div class="flex flex-row -mx-3">
                   <div class="flex-none w-2/3 max-w-full px-3">
                     <div>
-                      <p class="mb-0 font-sans text-sm font-semibold leading-normal">{{ __('total_cash_added') }}</p>
+                      <p class="mb-0 font-sans text-sm font-semibold leading-normal">{{ __('total_remaining') }}</p>
                       <h5 class="mb-0 font-bold">
-                        Rs {{ number_format($totalCashAdded, 2) }}
+                        Rs {{ number_format($totalRemaining, 2) }}
                       </h5>
                     </div>
                   </div>
                   <div class="px-3 text-right basis-1/3">
-                    <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-green-700 to-emerald-500">
-                      <i class="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
+                    <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
+                      <i class="ni leading-none ni-wallet-43 text-lg relative top-3.5 text-white"></i>
                     </div>
                   </div>
                 </div>
@@ -56,22 +56,22 @@
             </div>
           </div>
 
-          <!-- Total Remaining -->
+          <!-- Total Cash Added -->
           <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
             <div class="relative flex flex-col min-w-0 break-words bg-white shadow-soft-xl rounded-2xl bg-clip-border">
               <div class="flex-auto p-4">
                 <div class="flex flex-row -mx-3">
                   <div class="flex-none w-2/3 max-w-full px-3">
                     <div>
-                      <p class="mb-0 font-sans text-sm font-semibold leading-normal">{{ __('total_remaining') }}</p>
+                      <p class="mb-0 font-sans text-sm font-semibold leading-normal">{{ __('total_cash_added') }}</p>
                       <h5 class="mb-0 font-bold">
-                        Rs {{ number_format($totalRemaining, 2) }}
+                        Rs {{ number_format($totalCashAdded, 2) }}
                       </h5>
                     </div>
                   </div>
                   <div class="px-3 text-right basis-1/3">
-                    <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-purple-700 to-pink-500">
-                      <i class="ni leading-none ni-wallet-43 text-lg relative top-3.5 text-white"></i>
+                    <div class="inline-block w-12 h-12 text-center rounded-lg bg-gradient-to-tl from-green-700 to-emerald-500">
+                      <i class="ni leading-none ni-money-coins text-lg relative top-3.5 text-white"></i>
                     </div>
                   </div>
                 </div>
